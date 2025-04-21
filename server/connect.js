cbb816ee796fa15b937e1bd8424166b1203bab08
Gabriele <gabrielepinheirosa@gmail.com>
2025-04-20 23:59:14 -0300

    Atualizando conexão com o banco de dados

--- server/connect.js
+++ server/connect.js
@@ -1,8 +1,10 @@
 import mysql from "mysql2";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const db = mysql.createPool({ 
     host: "localhost",
-    user: "root",
-    password: "senha", 
-    database: "banco",
+    user: process.env.db_user,
+    password: process.env.db_pass, 
+    database: process.env.db_schema,
 });
